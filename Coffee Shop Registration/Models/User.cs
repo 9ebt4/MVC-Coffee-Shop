c57856adf685785ebe6ee175be1911e8f1d44c5b
ethan <ethanbjansen@gmail.com>
2023-02-10 11:35:58 -0600

    initial

---
+++ Coffee Shop Registration/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee_Shop_Registration.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        private string Password { get; set; }
+    }
+}
